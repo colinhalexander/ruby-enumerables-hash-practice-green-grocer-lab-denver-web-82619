602f08bbc4732a49abc5691ecd610c671be7c37b
Colin Alexander <chalexander@me.com>
2019-08-22 20:53:42 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -20,7 +20,7 @@ def apply_coupons(cart, coupons)
   if matching_coupons.length == 0 
     return cart
   end
-  #apply matching coupon to item
+
   i = 0
   while i < matching_coupons.length do
     item = "#{matching_coupons[i][:item]}"
@@ -36,15 +36,6 @@ def apply_coupons(cart, coupons)
         cart["#{item} W/COUPON"][:price] = price_per_item
         
         cart.delete(item)
-      else
-        cart["#{item} W/COUPON"] = cart[item]
-        
-        price_per_item = matching_coupons[i][:cost] / matching_coupons[i][:num]
-        cart["#{item} W/COUPON"][:price] = price_per_item
-        
-        cart[item][:count] = (cart_count % coupon_num)
-        cart["#{item} W/COUPON"][:count] -= (cart_count % coupon_num)
-      end
     end
     i += 1
   end
