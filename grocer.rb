c245107b8b0d9abc5f2c5e5743515477876d2400
Colin Alexander <chalexander@me.com>
2019-08-22 19:24:58 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -2,7 +2,7 @@ def consolidate_cart(cart)
   #translate array of hashes (cart) into a hash
   hash_cart = cart.reduce({}) do |memo, pair|
     pair.each do |pair|
-      
+      p pair
     end
     memo
   end
