8dc1ad64af0547ded98475e3f4b6853276131e21
Colin Alexander <chalexander@me.com>
2019-08-22 18:46:24 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,10 @@
 def consolidate_cart(cart)
   #translate array of hashes (cart) into a hash
-  hash_cart = cart.reduce({}) do |memo, |
+  hash_cart = cart.reduce({}) do |memo, pair|
     memo[key] = value
+    memo
   end
-
+  p hash_cart
   #add a count of one to single items
   
   #increment count for duplicates
